9f6a73982969b5a13ea8ac886caf75366b869fbf
Julio Romero <julioale04031981@gmail.com>
2021-02-28 20:24:44 -0300

    chore: extracted body of view to a component

---
+++ src/components/Body.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="todo-body">
+    <div class="todo-content d-flex flex-column align-items-center">
+      <TodoForm />
+      <TodoList />
+
+      <div v-if="todos.length" class="todo-control">
+        <TodoListFooter />
+        <TodoListFilter class="d-md-none" />
+      </div>
+
+      <span v-if="todos.length" class="drag-message">
+        <small>Drag and drop to reorder list</small>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+import TodoList from "../components/todo/TodoList";
+import TodoForm from "../components/todo/TodoForm.vue";
+import TodoListFooter from "../components/todo/TodoListFooter";
+import TodoListFilter from "../components/todo/TodoListFilter";
+
+export default {
+  components: { TodoList, TodoForm, TodoListFooter, TodoListFilter },
+  setup() {
+    const todos = inject("todos");
+
+    return { todos };
+  }
+}
+</script>
+
+<style scoped>
+.todo-body {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.todo-content {
+  margin-top: -6.5rem;
+  min-width: 328px;
+}
+
+.todo-content-title {
+  color: var(--light-grayish-blue-hover-color);
+  letter-spacing: 1rem;
+  font-weight: 700;
+}
+
+@media screen and (min-width: 768px) {
+  .todo-content {
+    min-width: 541px;
+    margin-top: -9rem;
+  }
+}
+</style>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="d-flex flex-column">
     <Header />
-
-    <div class="todo-body">
-      <div class="todo-content d-flex flex-column align-items-center">
-        <TodoForm />
-        <TodoList />
-
-        <div v-if="todos.length" class="todo-control">
-          <TodoListFooter />
-          <TodoListFilter class="d-md-none" />
-        </div>
-
-        <span v-if="todos.length" class="drag-message">
-          <small>Drag and drop to reorder list</small>
-        </span>
-      </div>
-    </div>
+    <Body />
   </div>
 </template>
 
 <script>
 import { provide, ref, watchEffect } from "vue";
 import Header from "../components/Header";
-import TodoList from "../components/todo/TodoList";
-import TodoForm from "../components/todo/TodoForm.vue";
-import TodoListFooter from "../components/todo/TodoListFooter";
-import TodoListFilter from "../components/todo/TodoListFilter";
+import Body from "../components/Body";
 
 export default {
   name: "TodoView",
-  components: { Header, TodoList, TodoForm, TodoListFooter, TodoListFilter },
+  components: { Header, Body },
   setup() {
     const todos = ref([]);
     const status = ref("all");
@@ -53,30 +35,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.todo-body {
-  min-height: 100vh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-
-.todo-content {
-  margin-top: -6.5rem;
-  min-width: 328px;
-}
-
-.todo-content-title {
-  color: var(--light-grayish-blue-hover-color);
-  letter-spacing: 1rem;
-  font-weight: 700;
-}
-
-@media screen and (min-width: 768px) {
-  .todo-content {
-    min-width: 541px;
-    margin-top: -9rem;
-  }
-}
-</style>
